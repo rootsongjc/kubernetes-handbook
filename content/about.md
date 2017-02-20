6e557cde21c994dec14223a75fa473617f12cf7f
Jimmy Song <rootsongjc@gmail.com>
2017-02-20 21:24:53 +0800

    first commit

---
+++ content/about.md
@@ -0,0 +1,10 @@
++++
+draft = true
+date = "2017-02-20T20:10:56+08:00"
+title = "about"
+
++++
+# About me
+Jimmy Song
+rootsongjc@gmail.com
+
---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+msg="rebuilding site `date`"
+if [ $# -eq 1  ]
+      then msg="$1"
+      fi
+
+      git add -A
+      git commit -m "$msg"
+      git push origin master
+
+
+      # Build the project. 
+      hugo # if using a theme, replace by `hugo -t <yourtheme>`
+
+      # Go To Public folder
+      cd public
+      # Add changes to git.
+      git add -A
+
+      # Commit changes.
+
+      git commit -m "$msg"
+
+      # Push source and build repos.
+      git push origin master
+
+      # Come Back
+      cd ..
+"]]"
