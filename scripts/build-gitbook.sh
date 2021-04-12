e76c601c08512e7624a147090fb476c30503cc4e
Jimmy Song <rootsongjc@gmail.com>
2021-04-13 00:09:04 +0800

    update

--- scripts/build-gitbook.sh
+++ scripts/build-gitbook.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 gitbook install
+# install sitemap-gerneral gitbook plugin
+npm install -g gitbook-plugin-sitemap-general
 gitbook build
 cp images/apple-touch-icon-precomposed-152.png _book/gitbook/images
