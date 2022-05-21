f4643d6ad4da180ffe0826f8aae0c8d48507761f
Jimmy Song <rootsongjc@gmail.com>
2022-05-21 09:30:07 +0800

    update script

--- update-algolia.sh
+++ update-algolia.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --env APPLICATION_ID=CRNDR5CNMU --env API_KEY=${API_KEY} --env "CONFIG=$(cat algolia-config.json | jq -r tostring)" algolia/docsearch-scraper
+docker run --env APPLICATION_ID=CRNDR5CNMU --env API_KEY=${API_KEY} --env "CONFIG=$(cat algolia-config.json | jq -r tostring)" algolia/docsearch-scraper
